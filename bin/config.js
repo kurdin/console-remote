4734f23ead2ef0c2d2e05359c36609db1d7c8ad5
Sergey Kurdin <sergey@webcorrector.co>
2013-12-04 16:11:17 -0500

    production server name fix

--- bin/config.js
+++ bin/config.js
@@ -1,5 +1,5 @@
 // config for console.re CLI (command line interface) please change YOUR-CHANNEL-NAME to your own unique channel name
 process.env.SERVER_CHANNEL = 'YOUR-CHANNEL-NAME';
-process.env.SERVER_DOMAIN = 'console.dev';
+process.env.SERVER_DOMAIN = 'console.re';
 process.env.SERVER_PORT = '80';
 
--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config for Console.Re API Test 
 process.env.SERVER_CHANNEL = 'YOUR-CHANNEL-NAME';
-process.env.SERVER_DOMAIN = 'console.dev';
+process.env.SERVER_DOMAIN = 'console.re';
 process.env.SERVER_PORT = '80';
 
