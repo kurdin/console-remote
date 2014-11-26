5493cabeaa63df6a8c97a1d87b832582866c04dc
Sergey Kurdin <sergey@webcorrector.co>
2014-11-26 17:58:52 -0500

    https fix for port 443

--- bin/config.js
+++ bin/config.js
@@ -2,5 +2,5 @@
 process.env.SERVER_CHANNEL = 'YOUR-CHANNEL-NAME';
 process.env.SERVER_DOMAIN = 'console.re';
 // if SERVER_PORT is 433 then console.re will use https:// to connect
-process.env.SERVER_PORT = '80';
+// process.env.SERVER_PORT = '80';
 
