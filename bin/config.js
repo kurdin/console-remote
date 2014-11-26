438bc3b7b4530d679b339a4d386ac09e83c095c9
Sergey Kurdin <sergey@webcorrector.co>
2014-11-26 17:47:58 -0500

    socket io version update

--- bin/config.js
+++ bin/config.js
@@ -1,5 +1,6 @@
 // config for console.re CLI (command line interface) please change YOUR-CHANNEL-NAME to your own unique channel name
 process.env.SERVER_CHANNEL = 'YOUR-CHANNEL-NAME';
 process.env.SERVER_DOMAIN = 'console.re';
-process.env.SERVER_PORT = '443';
+// if SERVER_PORT is 433 then console.re will use https:// to connect
+process.env.SERVER_PORT = '80';
 
