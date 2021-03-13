22665e651a59a4053e7b7bde86b89c6ef8bbe4cf
Sergey Kurdin <kurdiny@gmail.com>
2021-03-13 09:00:15 -0500

    version banner added to connector

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,34 @@
+const webpack = require('webpack')
+const TerserPlugin = require('terser-webpack-plugin')
+const PACKAGE = require('./package.json')
+const banner = PACKAGE.name + ' - ' + PACKAGE.version
+
 module.exports = {
   output: {
     library: 'consolere',
     libraryTarget: 'umd',
     filename: 'connector.js',
-    globalObject: 'self',
+    globalObject: 'self'
+  },
+  optimization: {
+    minimize: process.env.NODE_ENV === 'production',
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false
+      })
+    ]
   },
   name: 'connector',
   devtool: process.env.NODE_ENV === 'production' ? false : 'source-map',
   entry: './lib/index.js',
   mode: process.env.NODE_ENV || 'development',
-};
+  plugins: [
+    new webpack.BannerPlugin(banner),
+    new webpack.DefinePlugin({
+      CONSOLE_VERSION: JSON.stringify(PACKAGE.version),
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development')
+      }
+    })
+  ]
+}
